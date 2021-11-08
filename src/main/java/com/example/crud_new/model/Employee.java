b3f4566026fb63f358d55dfd9012cee83137fa45
Chau Quoc An <chauquocan2090@gmail.com>
2021-11-08 11:32:08 +0700

    commit set

--- src/main/java/com/example/crud_new/model/Employee.java
+++ src/main/java/com/example/crud_new/model/Employee.java
@@ -12,4 +12,29 @@ import java.io.Serializable;
 public class Employee implements Serializable {
     private int id;
     private String name;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Employee other = (Employee) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+    public Employee(int id) {
+        super();
+        this.id = id;
+    }
 }
--- src/main/java/com/example/crud_new/repository/EmployeeRepository.java
+++ src/main/java/com/example/crud_new/repository/EmployeeRepository.java
@@ -4,9 +4,11 @@ import com.example.crud_new.model.Employee;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public class EmployeeRepository {
@@ -15,35 +17,56 @@ public class EmployeeRepository {
 
     private ListOperations listOperations;//curd list
 
+    private SetOperations setOperations;//crud set
+
     private RedisTemplate redisTemplate;
 
     public EmployeeRepository(RedisTemplate redisTemplate) {
 //        this.hashOperations = redisTemplate.opsForHash();//hash
-        this.listOperations = redisTemplate.opsForList();//list
+
+//        this.listOperations = redisTemplate.opsForList();//list
+
+        this.setOperations = redisTemplate.opsForSet();//set
+
         this.redisTemplate = redisTemplate;
 
     }
 
     public void saveEmployee(Employee employee){
 //        hashOperations.put("EMPLOYEE", employee.getId(), employee);//hash
-        listOperations.leftPush("EMPLOYEE",employee);//list
+
+//        listOperations.leftPush("EMPLOYEE",employee);//list
+
+        setOperations.add("EMPLOYEE",employee);//set
 
     }
-    public List<Employee> findAll(){
-//        return hashOperations.values("EMPLOYEE");
-        return listOperations.range("EMPLOYEE",0,listOperations.size("EMPLOYEE"));
+    public Set<Employee> findAll(){
+//        return hashOperations.values("EMPLOYEE");//hash
+
+//        return listOperations.range("EMPLOYEE",0,listOperations.size("EMPLOYEE"));//list
+
+        return setOperations.members("EMPLOYEE");
     }
     public Employee findById(Integer id){
 //        return (Employee) hashOperations.get("EMPLOYEE", id);//hash
+
         //list
-        List<Employee> list = listOperations.range("EMPLOYEE",0,listOperations.size("EMPLOYEE"));
-        for (Employee employee : list){
+//        List<Employee> list = listOperations.range("EMPLOYEE",0,listOperations.size("EMPLOYEE"));
+////        for (Employee employee : list){
+////            if(employee.getId()==id){
+////                return employee;
+////            }
+////        }
+////        return null;
+
+        //set
+        Set<Employee> set = this.findAll();
+        for (Employee employee: set){
             if(employee.getId()==id){
                 return employee;
             }
         }
         return null;
-        //
     }
 
     public void update(Employee employee){
@@ -52,6 +75,10 @@ public class EmployeeRepository {
 
     public void delete(Integer id){
 //        hashOperations.delete("EMPLOYEE", id);//hash
-        listOperations.rightPopAndLeftPush("EMPLOYEE",id);//list
+
+//        listOperations.rightPopAndLeftPush("EMPLOYEE",id);//list
+
+        Employee employee = this.findById(id);
+        setOperations.remove("EMPLOYEE",id);
     }
 }
